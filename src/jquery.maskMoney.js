3353d5d46b35c0e596bb9a2b9d6806b8aaa9b517
Manoel Dinab <dinabjunior@hotmail.com>
2018-03-15 16:16:44 -0300

    Correção na utilização da numero com precision zero

--- src/jquery.maskMoney.js
+++ src/jquery.maskMoney.js
@@ -51,18 +51,21 @@
 
         unmasked: function () {
             return this.map(function () {
-                var value = ($(this).val() || "0"),
-                    isNegative = value.indexOf("-") !== -1,
-                    decimalPart;
+                var value = ($(this).val() || "0"), isNegative = value.indexOf("-") !== -1, decimalPart;
                 // get the last position of the array that is a number(coercion makes "" to be evaluated as false)
-                $(value.split(/\D/).reverse()).each(function (index, element) {
-                    if (element) {
-                        decimalPart = element;
-                        return false;
-                    }
-                });
-                value = value.replace(/\D/g, "");
-                value = value.replace(new RegExp(decimalPart + "$"), "." + decimalPart);
+                var isDecimal = value.split(/\D/).length > 1;
+                if(isDecimal) {
+                    $(value.split(/\D/).reverse()).each(function (index, element) {
+                        if (element) {
+                            decimalPart = element;
+                            return false;
+                        }
+                    });
+                    
+                    value = value.replace(/\D/g, "");
+                    value = value.replace(new RegExp(decimalPart + "$"), "." + decimalPart);
+                }
+
                 if (isNegative) {
                     value = "-" + value;
                 }
